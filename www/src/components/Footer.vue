229e9d9c2ef291ed772bead452eb12f0f3952162
Susan Woods <swoods63@msn.com>
2017-06-15 14:10:00 -0600

    Added property and proverties

--- www/src/components/Footer.vue
+++ www/src/components/Footer.vue
@@ -1,21 +1,24 @@
 <template>
+  <div class="component">
+
+
+  </div>
 </template>
 
 
-
-
-
-
-
 <script>
+export default {
+  name: 'component',
+  data(){
+
+  },
+  computed:{},
+  methods:{},
+  components:{}
+}
 </script>
 
 
-
-
-
-
-
-
 <style scoped>
+
 </style>
--- www/src/components/Property.vue
+++ www/src/components/Property.vue
@@ -1,22 +1,22 @@
-// <template>
-//   <div>
-//     Active Property: {{property}}
-//   </div>
-// </template>
+<!--<template>
+  <div>
+    Active Property: {{property}}
+  </div>
+</template>
 
-// <script>
-// export default {
-//   name: 'property',
-//   mounted(){
-//     this.$root.$data.store.actions.getProperty(this.$route.params.id)
-//   },
-//   computed:{
-//     property(){
-//       return this.$root.$data.store.state.activeProperty
-//     }
-//   }
-// }
-// </script>
+<script>
+export default {
+  name: 'property',
+  mounted(){
+    this.$root.$data.store.actions.getProperty(this.$route.params.id)
+  },
+  computed:{
+    property(){
+      return this.$root.$data.store.state.activeProperty
+    }
+  }
+}
+</script>
 
-// <style scoped>
-// </style>
+<style scoped>
+</style>-->
