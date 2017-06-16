06d132e9bd027b8c95c41697341310c6a15d68b8
Susan Woods <swoods63@msn.com>
2017-06-16 15:28:21 -0600

    Add components in Vue

--- www/src/components/Login.vue
+++ www/src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="footer">
+  <div class="login">
 
 
   </div>
@@ -8,7 +8,7 @@
 
 <script>
 export default {
-  name: 'footer',
+  name: "login",
   data(){
 
   },
--- www/src/components/Privacy.vue
+++ www/src/components/Privacy.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="footer">
+  <div class="privacy-policy">
 
 
   </div>
@@ -8,7 +8,7 @@
 
 <script>
 export default {
-  name: 'footer',
+  name: 'privacy-policy',
   data(){
 
   },
