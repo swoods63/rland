4c27d584a9777b3181a0b4913dbc976049d23f18
Susan Woods <swoods63@msn.com>
2017-06-28 09:59:30 -0600

    syn with Jeff

--- server/authentication/sessions.js
+++ server/authentication/sessions.js
@@ -4,23 +4,22 @@ import env from '../config/env'
 
 console.log(process.env.CONNECTIONSTRING)
 
-let store = new MongoDBStore(
-	{
-		uri: process.env.CONNECTIONSTRING,
-		collection: 'Sessions'
-	});
+let store = new MongoDBStore({
+    uri: process.env.CONNECTIONSTRING,
+    collection: 'Sessions'
+});
 
 // Catch errors 
-store.on('error', function (error) {
-	console.error(error);
+store.on('error', function(error) {
+    console.error(error);
 });
 
 module.exports = session({
-	secret: 'It\'s dangerous to go alone',
-	cookie: {
-		maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week 
-	},
-	store: store,
-	resave: true,
-	saveUninitialized: true
+    secret: 'It\'s dangerous to go alone',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week 
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true
 })
--- server/config/env.js
+++ server/config/env.js
@@ -8,20 +8,20 @@ let env = {
     DBHOST: 'ds123752.mlab.com:23752',
     DBNAME: 'rland',
     SERVERNAME: 'dev-server'
-}
+};
 
 // MAPS env TO ACTUAL ENVIRONMENT
 
 Object.keys(env).forEach(v => {
 
-    process.env[v] = process.env[v] || env[v]
+    process.env[v] = process.env[v] || env[v];
 
-})
+});
 
 
 // MongoDb Connection String Builder
 
-env.CONNECTIONSTRING = `${env.DBPROTOCOL}://${env.DBUSERNAME}:${env.DBPASSWORD}@${env.DBHOST}/${env.DBNAME}`
-process.env.CONNECTIONSTRING = env.CONNECTIONSTRING
+env.CONNECTIONSTRING = `${env.DBPROTOCOL}://${env.DBUSERNAME}:${env.DBPASSWORD}@${env.DBHOST}/${env.DBNAME}`;
+process.env.CONNECTIONSTRING = env.CONNECTIONSTRING;
 
-exports = env
+exports = env;
