2c2fa418dc54af02446da32be837f0ddf4e75915
Susan Woods <swoods63@msn.com>
2017-06-29 14:06:37 -0600

    Sync

--- www/src/main.js
+++ www/src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import io from 'socket.io-client';
-import store from '../store';
+import store from './store';
 
 
 
